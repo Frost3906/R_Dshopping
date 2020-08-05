4fca9a4649c525b070a59e56fbb412a34874485d
students <students@0-00>
2020-08-05 15:28:02 +0900

    1차 수정

--- src/main/java/com/spring/controller/BoardController.java
+++ src/main/java/com/spring/controller/BoardController.java
@@ -1,19 +1,11 @@
 package com.spring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.spring.service.BoardService;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Controller
-@RequestMapping("/board/*")
+//@Slf4j
+//@Controller
+//@RequestMapping("/board/*")
 public class BoardController {
 	
-	@Autowired
-	private BoardService service;
+//	@Autowired
+//	private BoardService service;
 	
 }
